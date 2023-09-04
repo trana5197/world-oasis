39f6e6a7f18cd4234eecdf437c2040161dc5cbf2
rana <trana51997@gmail.com>
2023-09-03 21:04:31 -0500

    Done

--- src/features/authentication/LoginForm.jsx
+++ src/features/authentication/LoginForm.jsx
@@ -7,8 +7,8 @@ import { useLogin } from "./useLogin";
 import SpinnerMini from "../../ui/SpinnerMini";
 
 function LoginForm() {
-  const [email, setEmail] = useState("legox32468@stypedia.com");
-  const [password, setPassword] = useState("qwertyuiop[]");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
   const { login, isLogging } = useLogin();
 
